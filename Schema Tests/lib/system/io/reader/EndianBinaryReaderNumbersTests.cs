13020275adf131fdf428b37dfa1728b1fcc2f82e
MeltyPlayer <mastergamepro@hotmail.com>
2023-05-18 18:34:14 -0500

    Added more number tests for EndianBinaryReader.

--- Schema Tests/lib/system/io/reader/EndianBinaryReaderNumbersTests.cs
+++ Schema Tests/lib/system/io/reader/EndianBinaryReaderNumbersTests.cs
@@ -16,7 +16,7 @@ namespace System.IO {
     }
 
     [Test]
-    [TestCase(new byte[] { 0x00, 0x00 }, (ushort) 0)]
+    [TestCase(new byte[] { 0x00, 0x00 }, ushort.MinValue)]
     [TestCase(new byte[] { 0xFF, 0xFF }, ushort.MaxValue)]
     [TestCase(new byte[] { 0xFF, 0x7F }, (ushort) 32767)]
     [TestCase(new byte[] { 0x00, 0x80 }, (ushort) 32768)]
@@ -50,6 +50,56 @@ namespace System.IO {
       using var er = new EndianBinaryReader(ms, Endianness.LittleEndian);
       Assert.AreEqual(expectedValue, er.ReadUInt24());
       Assert.AreEqual(3, ms.Position);
+    }
+
+
+    [Test]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00 }, 0)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF }, -1)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0x7F }, int.MaxValue)]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x80 }, int.MinValue)]
+    public void TestReadInt32(byte[] bytes, int expectedValue) {
+      using var ms = new MemoryStream(bytes);
+      using var er = new EndianBinaryReader(ms, Endianness.LittleEndian);
+      Assert.AreEqual(expectedValue, er.ReadInt32());
+      Assert.AreEqual(4, ms.Position);
+    }
+
+    [Test]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00 }, uint.MinValue)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF }, uint.MaxValue)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0x7F }, (uint) 2147483647)]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x80 }, (uint) 2147483648)]
+    public void TestReadUInt32(byte[] bytes, uint expectedValue) {
+      using var ms = new MemoryStream(bytes);
+      using var er = new EndianBinaryReader(ms, Endianness.LittleEndian);
+      Assert.AreEqual(expectedValue, er.ReadUInt32());
+      Assert.AreEqual(4, ms.Position);
+    }
+
+
+    [Test]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, 0)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }, -1)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F }, long.MaxValue)]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80 }, long.MinValue)]
+    public void TestReadInt64(byte[] bytes, long expectedValue) {
+      using var ms = new MemoryStream(bytes);
+      using var er = new EndianBinaryReader(ms, Endianness.LittleEndian);
+      Assert.AreEqual(expectedValue, er.ReadInt64());
+      Assert.AreEqual(8, ms.Position);
+    }
+
+    [Test]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, ulong.MinValue)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }, ulong.MaxValue)]
+    [TestCase(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F }, (ulong) 9223372036854775807)]
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80 }, (ulong) 9223372036854775808)]
+    public void TestReadUInt64(byte[] bytes, ulong expectedValue) {
+      using var ms = new MemoryStream(bytes);
+      using var er = new EndianBinaryReader(ms, Endianness.LittleEndian);
+      Assert.AreEqual(expectedValue, er.ReadUInt64());
+      Assert.AreEqual(8, ms.Position);
     }
   }
 }
