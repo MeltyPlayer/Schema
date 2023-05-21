181998fb61e93ca8b95954cca559acf2a8bb3c73
MeltyPlayer <mastergamepro@hotmail.com>
2023-05-21 01:12:15 -0500

    Fixed a dumb bug that was breaking the package (the new Span methods were not inlcuded in the interface).

---
+++ Schema Tests/binary/build/SequenceBuildTests.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+using schema.binary.attributes.sequence;
+
+namespace schema.binary.build {
+  public partial class SequenceBuildTests {
+    [BinarySchema]
+    public partial class SequenceWrapper : IBinaryConvertible {
+      [SequenceLengthSource(SchemaIntegerType.BYTE)]
+      public int[] Values { get; set; }
+
+      public override bool Equals(object other) {
+        if (other is SequenceWrapper otherSequenceWrapper) {
+          return this.Values.SequenceEqual(otherSequenceWrapper.Values);
+        }
+
+        return false;
+      }
+    }
+
+    [Test]
+    public void TestWriteAndRead() {
+      var expectedSw = new SequenceWrapper {
+          Values = new[] { 1, 2, 3, 4, 5, 9, 8, 7, 6 }
+      };
+
+      var ms = new MemoryStream();
+
+      var endianness = Endianness.BigEndian;
+      var ew = new EndianBinaryWriter(endianness);
+
+      expectedSw.Write(ew);
+      ew.CompleteAndCopyToDelayed(ms).Wait();
+
+      ms.Position = 0;
+      var er = new EndianBinaryReader(ms, endianness);
+      var actualSw = er.ReadNew<SequenceWrapper>();
+
+      Assert.AreEqual(expectedSw, actualSw);
+    }
+  }
+}
--- Schema/lib/system/io/reader/IEndianBinaryReader.cs
+++ Schema/lib/system/io/reader/IEndianBinaryReader.cs
@@ -25,73 +25,91 @@ namespace System.IO {
 
     byte[] ReadBytes(long count);
     void ReadBytes(byte[] dst, int start, int length);
+    void ReadBytes(Span<byte> dst);
 
     sbyte[] ReadSBytes(long count);
     void ReadSBytes(sbyte[] dst, int start, int length);
+    void ReadSBytes(Span<sbyte> dst);
 
     short[] ReadInt16s(long count);
     void ReadInt16s(short[] dst, int start, int length);
+    void ReadInt16s(Span<short> dst);
 
     ushort[] ReadUInt16s(long count);
     void ReadUInt16s(ushort[] dst, int start, int length);
+    void ReadUInt16s(Span<ushort> dst);
 
     void AssertInt24(int expectedValue);
     int ReadInt24();
     int[] ReadInt24s(long count);
     void ReadInt24s(int[] dst, int start, int length);
+    void ReadInt24s(Span<int> dst);
 
     void AssertUInt24(uint expectedValue);
     uint ReadUInt24();
     uint[] ReadUInt24s(long count);
     void ReadUInt24s(uint[] dst, int start, int length);
+    void ReadUInt24s(Span<uint> dst);
 
     int[] ReadInt32s(long count);
     void ReadInt32s(int[] dst, int start, int length);
+    void ReadInt32s(Span<int> dst);
 
     uint[] ReadUInt32s(long count);
     void ReadUInt32s(uint[] dst, int start, int length);
+    void ReadUInt32s(Span<uint> dst);
 
     long[] ReadInt64s(long count);
     void ReadInt64s(long[] dst, int start, int length);
+    void ReadInt64s(Span<long> dst);
 
     ulong[] ReadUInt64s(long count);
     void ReadUInt64s(ulong[] dst, int start, int length);
+    void ReadUInt64s(Span<ulong> dst);
 
     void AssertHalf(float expectedValue);
     float ReadHalf();
     float[] ReadHalfs(long count);
     void ReadHalfs(float[] dst, int start, int length);
+    void ReadHalfs(Span<float> dst);
 
     float[] ReadSingles(long count);
     void ReadSingles(float[] dst, int start, int length);
+    void ReadSingles(Span<float> dst);
 
     double[] ReadDoubles(long count);
     void ReadDoubles(double[] dst, int start, int length);
+    void ReadDoubles(Span<double> dst);
 
     void AssertSn8(float expectedValue);
     float ReadSn8();
     float[] ReadSn8s(long count);
     void ReadSn8s(float[] dst, int start, int length);
+    void ReadSn8s(Span<float> dst);
 
     void AssertUn8(float expectedValue);
     float ReadUn8();
     float[] ReadUn8s(long count);
     void ReadUn8s(float[] dst, int start, int length);
+    void ReadUn8s(Span<float> dst);
 
     void AssertSn16(float expectedValue);
     float ReadSn16();
     float[] ReadSn16s(long count);
     void ReadSn16s(float[] dst, int start, int length);
+    void ReadSn16s(Span<float> dst);
 
     void AssertUn16(float expectedValue);
     float ReadUn16();
     float[] ReadUn16s(long count);
     void ReadUn16s(float[] dst, int start, int length);
+    void ReadUn16s(Span<float> dst);
 
     void AssertChar(Encoding encoding, char expectedValue);
     char ReadChar(Encoding encoding);
     char[] ReadChars(Encoding encoding, long count);
     void ReadChars(Encoding encoding, char[] dst, int start, int length);
+    void ReadChars(Encoding encoding, Span<char> dst);
 
     string ReadUpTo(char endToken);
     string ReadUpTo(Encoding encoding, char endToken);
