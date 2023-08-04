ba4393b3a33588580c8c4cc39685817bf05f1b1d
MeltyPlayer <mastergamepro@hotmail.com>
2023-08-04 01:00:37 -0500

    Added test coverage for classes with multiple generic arguments.

--- Schema Tests/binary/generator/GenericGeneratorTests.cs
+++ Schema Tests/binary/generator/GenericGeneratorTests.cs
@@ -4,7 +4,7 @@
 namespace schema.binary.text {
   internal class GenericGeneratorTests {
     [Test]
-    public void TestGenericStructure() {
+    public void Test1GenericArgumentClass() {
       BinarySchemaTestUtil.AssertGenerated(@"
 using schema.binary;
 
@@ -32,6 +32,46 @@ namespace foo.bar {
   public partial class GenericWrapper<T> {
     public void Write(ISubEndianBinaryWriter ew) {
       this.Data.Write(ew);
+    }
+  }
+}
+");
+    }
+
+    [Test]
+    public void Test2GenericArgumentClass() {
+      BinarySchemaTestUtil.AssertGenerated(@"
+using schema.binary;
+
+namespace foo.bar {
+  [BinarySchema]
+  public partial class GenericWrapper<T1, T2> : IBinaryConvertible 
+      where T1 : IBinaryConvertible, new(),
+      where T2 : IBinaryConvertible, new(){
+    public T1 Data1 { get; } = new();
+    public T2 Data2 { get; } = new();
+  }
+}",
+                                           @"using System;
+using System.IO;
+
+namespace foo.bar {
+  public partial class GenericWrapper<T1, T2> {
+    public void Read(IEndianBinaryReader er) {
+      this.Data1.Read(er);
+      this.Data2.Read(er);
+    }
+  }
+}
+",
+                                           @"using System;
+using System.IO;
+
+namespace foo.bar {
+  public partial class GenericWrapper<T1, T2> {
+    public void Write(ISubEndianBinaryWriter ew) {
+      this.Data1.Write(ew);
+      this.Data2.Write(ew);
     }
   }
 }
