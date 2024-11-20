7ccd471b47f35a67eb28eeba5ce0f169d50a050e
MeltyPlayer <meltyplayer@gmail.com>
2024-11-20 01:19:23 -0600

    Added tests for writing vectors, just in case.

--- Schema Build Tests/binary/KnownStructTests.cs
+++ Schema Build Tests/binary/KnownStructTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.IO;
+using System.Numerics;
 
 using NUnit.Framework;
 
@@ -19,6 +20,20 @@ public partial class KnownStructTests {
     Assert.AreEqual(new Vector2(123, 456), br.ReadNew<Vector2Wrapper>().Value);
   }
 
+  [Test]
+  public void TestWritingVector2() {
+    var bw = new SchemaBinaryWriter();
+    bw.WriteVector2(new Vector2(123, 456));
+
+    using var ms = new MemoryStream(); 
+    bw.CompleteAndCopyTo(ms);
+    ms.Position = 0;
+
+    var br = new SchemaBinaryReader(ms);
+    Assert.AreEqual(123f, br.ReadSingle());
+    Assert.AreEqual(456f, br.ReadSingle());
+  }
+
 
   [BinarySchema]
   private partial class Vector4Wrapper : IBinaryConvertible {
@@ -30,5 +45,21 @@ public partial class KnownStructTests {
     using var br = SchemaMemoryStream.From([12f, 23f, 34f, 45f])
                                      .GetBinaryReader();
     Assert.AreEqual(new Vector4(12, 23, 34, 45), br.ReadNew<Vector4Wrapper>().Value);
+  }
+
+  [Test]
+  public void TestWritingVector4() {
+    var bw = new SchemaBinaryWriter();
+    bw.WriteVector4(new Vector4(12, 23, 34, 45));
+
+    using var ms = new MemoryStream(); 
+    bw.CompleteAndCopyTo(ms);
+    ms.Position = 0;
+
+    var br = new SchemaBinaryReader(ms);
+    Assert.AreEqual(12f, br.ReadSingle());
+    Assert.AreEqual(23f, br.ReadSingle());
+    Assert.AreEqual(34f, br.ReadSingle());
+    Assert.AreEqual(45f, br.ReadSingle());
   }
 }
