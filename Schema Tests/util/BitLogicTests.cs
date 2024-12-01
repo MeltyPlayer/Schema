3e3c71af521425c2569b78ba09bd15ad5a53611e
MeltyPlayer <meltyplayer@gmail.com>
2024-12-01 11:50:07 -0600

    Added tests for the previous commit.

--- Schema Tests/util/BitLogicTests.cs
+++ Schema Tests/util/BitLogicTests.cs
@@ -13,4 +13,13 @@ public class BitLogicTests {
   [TestCase((uint) 17, ExpectedResult = (uint) 3)]
   public uint TestBytesNeededToContainBits(uint bits)
     => BitLogic.BytesNeededToContainBits(bits);
+
+  [Test]
+  [TestCase((uint) 0, ExpectedResult = 0.0)]
+  [TestCase((uint) 1, ExpectedResult = 0.000244140625)]
+  [TestCase((uint) 100, ExpectedResult = 0.0244140625)]
+  [TestCase(0xF0000000, ExpectedResult = -65536)]
+  [TestCase(uint.MaxValue, ExpectedResult = -0.000244140625)]
+  public double TestFixed32_1_19_12(uint x)
+    => BitLogic.GetFixedPointDouble(x, 1, 19, 12);
 }
