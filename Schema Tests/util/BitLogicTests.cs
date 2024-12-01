c485be4e129a3860f81d39ef307dd4c0bb85faa3
MeltyPlayer <meltyplayer@gmail.com>
2024-12-01 11:39:45 -0600

    Set up a method for getting how many bytes need to be read to read some number of bits.

---
+++ Schema Tests/util/BitLogicTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+
+namespace schema.util;
+
+public class BitLogicTests {
+  [Test]
+  [TestCase((uint) 0, ExpectedResult = (uint) 0)]
+  [TestCase((uint) 1, ExpectedResult = (uint) 1)]
+  [TestCase((uint) 8, ExpectedResult = (uint) 1)]
+  [TestCase((uint) 9, ExpectedResult = (uint) 2)]
+  [TestCase((uint) 16, ExpectedResult = (uint) 2)]
+  [TestCase((uint) 17, ExpectedResult = (uint) 3)]
+  public uint TestBytesNeededToContainBits(uint bits)
+    => BitLogic.BytesNeededToContainBits(bits);
+}
---
+++ Schema/src/util/BitLogic.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace schema.util {
+  public static class BitLogic {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint BytesNeededToContainBits(uint bits) {
+      var floorBytes = bits >> 3;
+      return (bits - (floorBytes << 3)) == 0 ? floorBytes : floorBytes + 1;
+    }
+  }
+}
