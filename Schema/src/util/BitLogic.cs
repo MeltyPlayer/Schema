037e6600d54657261dfabd0b5a140972c51d3f87
MeltyPlayer <meltyplayer@gmail.com>
2024-12-01 11:50:26 -0600

    Whoops, everything's getting committed out of order.

--- Schema/src/util/BitLogic.cs
+++ Schema/src/util/BitLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace schema.util {
   public static class BitLogic {
@@ -6,6 +7,26 @@ namespace schema.util {
     public static uint BytesNeededToContainBits(uint bits) {
       var floorBytes = bits >> 3;
       return (bits - (floorBytes << 3)) == 0 ? floorBytes : floorBytes + 1;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double GetFixedPointDouble(uint x,
+                                             byte signBits,
+                                             byte integerBits,
+                                             byte fractionBits) {
+      double doubleValue;
+      if (signBits == 0) {
+        doubleValue = x;
+      } else {
+        var signMask = 1 << (integerBits + fractionBits);
+        if ((x & signMask) != 0) {
+          doubleValue = (double) (x | ~(signMask - 1));
+        } else {
+          doubleValue = x;
+        }
+      }
+
+      return doubleValue * Math.Pow(.5, fractionBits);
     }
   }
 }
