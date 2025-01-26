105feed9e1aeedc9e3d7329c0eb0574ce8536ad2
MeltyPlayer <meltyplayer@gmail.com>
2025-01-26 00:07:02 -0600

    Fixed bug introduced in my last fix.

--- Schema/src/text/reader/TextReaderConstants.cs
+++ Schema/src/text/reader/TextReaderConstants.cs
@@ -9,14 +9,13 @@ public sealed class TextReaderConstants {
 
   public static readonly char[] NEWLINE_CHARS = ['\n', '\r'];
 
-  public static readonly string[] NEWLINE_STRINGS
-      = NEWLINE_CHARS.Select(c => c.ToString()).ToArray();
+  public static readonly string[] NEWLINE_STRINGS = ["\n", "\r\n"];
 
   public static readonly char[] WHITESPACE_CHARS =
       TextReaderConstants.NEWLINE_CHARS.Concat([' ', '\t']).ToArray();
 
-  public static readonly string[] WHITESPACE_STRINGS
-      = WHITESPACE_CHARS.Select(c => c.ToString()).ToArray();
+  public static readonly string[] WHITESPACE_STRINGS =
+      TextReaderConstants.NEWLINE_STRINGS.Concat([" ", "\t"]).ToArray();
 
   public static readonly char[] TERMINATORS =
       TextReaderConstants.WHITESPACE_CHARS.Concat(COMMA_CHARS).ToArray();
