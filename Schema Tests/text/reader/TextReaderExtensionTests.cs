ea64b28a0cddfa02c29bcbe2398400d2f1940652
MeltyPlayer <meltyplayer@gmail.com>
2026-08-11 21:12:29 -0500

    Set up a helper method for reading arguments with the text reader.

---
+++ Schema Tests/text/reader/TextReaderExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+
+namespace schema.text.reader;
+
+internal class TextReaderExtensionTests {
+  [Test]
+  // Empty cases
+  [TestCase("", ExpectedResult = new string[] { })]
+  [TestCase(" \n  \t ", ExpectedResult = new string[] { })]
+  [TestCase(" ) ", ExpectedResult = new string[] { })]
+  // One argument cases
+  [TestCase("foo", ExpectedResult = new[] { "foo" })]
+  [TestCase("  \n foo  \t ", ExpectedResult = new[] { "foo" })]
+  [TestCase("foo )", ExpectedResult = new[] { "foo" })]
+  // Multiple argument cases
+  [TestCase("foo,bar,123,abc", ExpectedResult = new[] { "foo", "bar", "123", "abc" })]
+  [TestCase("foo,bar,123,abc,", ExpectedResult = new[] { "foo", "bar", "123", "abc" })]
+  [TestCase("  foo , bar , 123, abc  ", ExpectedResult = new[] { "foo", "bar", "123", "abc" })]
+  // Mid-space cases
+  [TestCase(" foo bar ", ExpectedResult = new[] { "foo bar" })]
+  [TestCase("foo \t\n bar", ExpectedResult = new[] { "foo \t\n bar" })]
+  // Quote cases
+  [TestCase("  'foo'  ", ExpectedResult = new[] { "'foo'" })]
+  [TestCase("'foo bar'", ExpectedResult = new[] { "'foo bar'" })]
+  [TestCase("'foo, bar'", ExpectedResult = new[] { "'foo, bar'" })]
+  [TestCase("'foo\"'", ExpectedResult = new[] { "'foo\"'" })]
+  [TestCase("\"foo bar\"", ExpectedResult = new[] { "\"foo bar\"" })]
+  [TestCase("`foo bar`", ExpectedResult = new[] { "`foo bar`" })]
+  public string[] TestReadArguments(string text) {
+    using var tr = TextSchemaTestUtil.CreateTextReader(text);
+    return tr.ReadArguments([','], [')']);
+  }
+}
---
+++ Schema/src/text/reader/TextReaderExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using schema.util.symbols;
+
+namespace schema.text.reader;
+
+public static partial class TextReaderExtensions {
+  public static string[] ReadArguments(
+      this ITextReader tr,
+      ReadOnlySpan<char> separators,
+      ReadOnlySpan<char> terminators) {
+    if (tr.Eof) {
+      return [];
+    }
+
+    var arguments = new List<string>();
+
+    var sb = new StringBuilder();
+    var internalWhitespaceSb = new StringBuilder();
+
+    char? inQuoteType = null;
+
+    do {
+      var c = tr.ReadChar();
+
+      if (inQuoteType == null) {
+        foreach (var terminatorC in terminators) {
+          if (c == terminatorC) {
+            --tr.Position;
+            goto End;
+          }
+        }
+
+        foreach (var separatorC in separators) {
+          if (c == separatorC) {
+            arguments.Add(sb.ToString());
+            sb.Clear();
+            internalWhitespaceSb.Clear();
+            goto Next;
+          }
+        }
+
+        if (c is ' ' or '\t' or '\n') {
+          if (sb.Length > 0) {
+            internalWhitespaceSb.Append(c);
+          }
+
+          continue;
+        }
+      }
+
+      if (c is '`' or '\'' or '"') {
+        if (inQuoteType == null) {
+          inQuoteType = c;
+        } else if (inQuoteType == c) {
+          inQuoteType = null;
+        }
+      }
+
+      if (internalWhitespaceSb.Length > 0) {
+        sb.Append(internalWhitespaceSb);
+        internalWhitespaceSb.Clear();
+      }
+
+      sb.Append(c);
+
+      Next: ;
+    } while (!tr.Eof);
+
+    End:
+    if (sb.Length > 0) {
+      arguments.Add(sb.ToString());
+    }
+
+    return arguments.ToArray();
+  }
+}
