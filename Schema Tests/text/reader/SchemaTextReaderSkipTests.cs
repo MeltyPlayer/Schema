000fd74893feead9c68a9e3fc28277906c737d9b
MeltyPlayer <meltyplayer@gmail.com>
2026-08-13 22:38:51 -0500

    Set up tests for skipping text.

---
+++ Schema Tests/text/reader/SchemaTextReaderSkipTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+
+namespace schema.text.reader;
+
+internal class SchemaTextReaderSkipTests {
+  [Test]
+  [TestCase("", ExpectedResult = "")]
+  [TestCase("foo", ExpectedResult = "")]
+  [TestCase("foo_bar", ExpectedResult = "_bar")]
+  [TestCase("foo_bar_abc", ExpectedResult = "_bar_abc")]
+  public string TestSkipUpToStartOfTerminator_Char(string text) {
+    using var tr = new SchemaTextReader(text);
+    tr.SkipUpToStartOfTerminator('_');
+    return tr.ReadRemainder();
+  }
+
+  [Test]
+  [TestCase("", ExpectedResult = "")]
+  [TestCase("foo", ExpectedResult = "")]
+  [TestCase("foo123bar", ExpectedResult = "123bar")]
+  [TestCase("foo123bar123abc", ExpectedResult = "123bar123abc")]
+  public string TestSkipUpToStartOfTerminator_String(string text) {
+    using var tr = new SchemaTextReader(text);
+    tr.SkipUpToStartOfTerminator("123");
+    return tr.ReadRemainder();
+  }
+
+  [Test]
+  [TestCase("", ExpectedResult = "")]
+  [TestCase("foo", ExpectedResult = "")]
+  [TestCase("foo_bar", ExpectedResult = "bar")]
+  [TestCase("foo_bar_abc", ExpectedResult = "bar_abc")]
+  public string TestSkipUpToAndPastTerminator_Char(string text) {
+    using var tr = new SchemaTextReader(text);
+    tr.SkipUpToAndPastTerminator('_');
+    return tr.ReadRemainder();
+  }
+
+  [Test]
+  [TestCase("", ExpectedResult = "")]
+  [TestCase("foo", ExpectedResult = "")]
+  [TestCase("foo123bar", ExpectedResult = "bar")]
+  [TestCase("foo123bar123abc", ExpectedResult = "bar123abc")]
+  public string TestSkipUpToAndPastTerminator_String(string text) {
+    using var tr = new SchemaTextReader(text);
+    tr.SkipUpToAndPastTerminator("123");
+    return tr.ReadRemainder();
+  }
+}
--- Schema Tests/text/reader/TextReaderExtensionTests_Skip.cs
+++ Schema Tests/text/reader/TextReaderExtensionTests_Skip.cs
@@ -7,6 +7,20 @@ internal partial class TextReaderExtensionTests {
   [Test]
   [TestCase("", ExpectedResult = "")]
   [TestCase("    ", ExpectedResult = "")]
+  [TestCase("    \n", ExpectedResult = "")]
+  [TestCase("foo", ExpectedResult = "")]
+  [TestCase("foo\nbar", ExpectedResult = "bar")]
+  [TestCase("foo\r\nbar", ExpectedResult = "bar")]
+  [TestCase("  foo  \n  bar  ", ExpectedResult = "  bar  ")]
+  public string TestSkipToEndOfLine(string text) {
+    using var tr = new SchemaTextReader(text);
+    tr.SkipToEndOfLine();
+    return tr.ReadRemainder();
+  }
+  
+  [Test]
+  [TestCase("", ExpectedResult = "")]
+  [TestCase("    ", ExpectedResult = "")]
   [TestCase("  foo  ", ExpectedResult = "foo  ")]
   [TestCase(" \n\t foo", ExpectedResult = "foo")]
   [TestCase("  // 123\n foo", ExpectedResult = "foo")]
